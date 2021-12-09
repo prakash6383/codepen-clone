5e5a8866e80be6e06045a565f7ec990796fa74c6
prakash <prakashvijay4964@gmail.com>
2021-12-09 17:50:12 +0530

    first commit

---
+++ src/components/Editor.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { Launch, TransitEnterexitSharp } from "@material-ui/icons";
+
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  return (
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+          className="expand-collapse-btn"
+        >
+          {open ? <TransitEnterexitSharp /> : <Launch />}
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
